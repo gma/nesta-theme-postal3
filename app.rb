58b9fb7b868807e940a66dfb4b76207ae99ee288
Graham Ashton <graham@effectif.com>
2010-06-17 11:27:11 +0100

    Simplified Rack::Static config.

--- app.rb
+++ app.rb
@@ -1,2 +1,1 @@
-use Rack::Static, :urls => ["/postal3/images", "/postal3/javascripts"],
-                  :root => "themes/postal3/public"
+use Rack::Static, :urls => ["/postal3"], :root => "themes/postal3/public"
