804d0c9d1a9799ef2cfe1b14fde9e13d422f4264
Graham Ashton <graham@effectif.com>
2010-11-14 23:59:19 +0000

    Support the new Sinatra::Base style app.

--- app.rb
+++ app.rb
@@ -1,1 +1,5 @@
-use Rack::Static, :urls => ["/postal3"], :root => "themes/postal3/public"
+module Nesta
+  class App
+    use Rack::Static, :urls => ["/postal3"], :root => "themes/postal3/public"
+  end
+end
